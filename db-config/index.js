42a613051df534f13f45d46d757350875bc3fe4f
Let0oro <oniiichan354@gmail.com>
2023-11-12 14:50:56 +0100

    CRUD functions assigned to each endpoint

--- db-config/index.js
+++ db-config/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries');
 
 const app = express();
 const port = 3000;
@@ -15,7 +16,16 @@ app.get('/', function responseApi(request, response) {
     response.json({info: 'Node.js, Express, and Postgres API'})
 });
 
+
+// Assing the CRUD functions for each endPoint
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+
 app.listen(port, function isListenedMessage () {
     console.log(`App running on port: ${port}.`)
 });
-
